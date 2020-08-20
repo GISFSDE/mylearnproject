191a0fc494161c89f1360faa7a4e9d1f8ce47564
passion7144 <714416426@qq.com>
2020-08-20 14:32:34 +0800

    lxl 8.20

--- src/main/java/pers/lxl/mylearnproject/javase/javabase/HelloWord.java
+++ src/main/java/pers/lxl/mylearnproject/javase/javabase/HelloWord.java
@@ -1,4 +1,8 @@
 package pers.lxl.mylearnproject.javase.javabase;
 
 public class HelloWord {
+    public static void main(String[] args) {
+        double d=3.1415926;
+        System.out.printf("%.2f\n",d);
+    }
 }
