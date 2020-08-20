681e6eda6bb1cca60fa4842aa1377e784b2334c7
passion7144 <714416426@qq.com>
2020-08-20 15:32:46 +0800

    lxl 8.20

--- src/main/java/pers/lxl/mylearnproject/javase/javabase/HelloWord.java
+++ src/main/java/pers/lxl/mylearnproject/javase/javabase/HelloWord.java
@@ -18,25 +18,39 @@ public class HelloWord {
 //            System.out.println(array[i]);
 //        }
 //       冒泡排序
-        System.out.println("排序前："+ Arrays.toString(array));
-        for (int i = 0; i <array.length-1 ; i++) {
-            for (int j = 0; j <array.length-i-1 ; j++) {
-                if (array[j]>array[j+1]){
-                    int tmp=array[j];
-                    array[j]=array[j+1];
+        System.out.println("排序前：" + Arrays.toString(array));
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j + 1];
                     array[j + 1] = tmp;
                 }
             }
         }
+        String[] array2 = {"dwdw", "wwddd", "adwddddd"};
+        Arrays.sort(array2);
         System.out.println(Arrays.toString(array));
-
-
-
-
-
-
-
-
+       // System.out.println(array2);//直接打印数组名显示数组本身对应的哈希码（内存地址）
+        System.out.println(Arrays.toString(array2));
+//      【多维数组】-------------------------------
+        int[][] dbarray = {{21, 232, 12}, {23, 32, 556}};
+        int[][][] tbarray = {
+                {{221, 454, 76, 8}, {21, 232, 12}, {23, 32, 556}},
+                {{221, 454, 76, 8}, {21, 232, 12}, {23, 32, 556}},
+                {{221, 454, 76, 8}, {21, 232, 12}, {23, 32, 556}}
+        };
+//        打印
+        for (int[] arr : dbarray
+        ) {
+            for (int n : arr
+            ) {
+                System.out.print(n);
+                System.out.print(",");
+            }
+            System.out.println();
+        }
+        System.out.println(Arrays.deepToString(dbarray)+"  "+Arrays.deepToString(tbarray));
 
 
 
