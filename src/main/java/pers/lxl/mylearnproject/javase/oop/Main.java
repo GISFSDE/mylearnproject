53b7bab552e54601eb783b6090943e0d0e9682f0
passion7144 <714416426@qq.com>
2020-08-20 16:24:46 +0800

    lxl 8.20 1.0

--- src/main/java/pers/lxl/mylearnproject/javase/oop/Main.java
+++ src/main/java/pers/lxl/mylearnproject/javase/oop/Main.java
@@ -1,4 +1,19 @@
 package pers.lxl.mylearnproject.javase.oop;
 
 public class Main {
+    public static void main(String[] args) {
+        //Construction Method
+        Person p1 = new Person("lxl1");
+        Person p2 = new Person("lxl2",21);
+        System.out.println(p1.getName()+p1.getAge());
+        System.out.println(p2.getName()+p2.getAge());
+        //Method
+        Person li=new Person();
+        Person xiao=new Person();
+        li.setName("lxl");
+        li.setName("xiao","mei");
+        System.out.println(li.getName()+xiao.getName());
+
+
+    }
 }
--- src/main/java/pers/lxl/mylearnproject/javase/oop/Person.java
+++ src/main/java/pers/lxl/mylearnproject/javase/oop/Person.java
@@ -3,6 +3,19 @@ package pers.lxl.mylearnproject.javase.oop;
 public class Person {
     private String name;
     private int age;
+//自定义构造方法，没有返回值（也没有void），调用构造方法，必须用new操作符。
+//    不编写会有默认构造方法，自定义后不会自动创建无参构造方法
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+//如果相同时有无参构造需自己写
+    public Person(){
+    }
 
     public String getName() {
         return name;
@@ -11,7 +24,10 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
-
+//    Overloading
+    public void setName(String name,String name2){
+        this.name=name+name2;
+    }
     public int getAge() {
         return age;
     }
