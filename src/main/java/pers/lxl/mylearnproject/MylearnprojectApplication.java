e197b7bf4b543f64f32c54bbcae3fb50da7e32d5
passion7144 <714416426@qq.com>
2020-08-20 11:14:46 +0800

    lxl 8.20

---
+++ src/main/java/pers/lxl/mylearnproject/MylearnprojectApplication.java
@@ -0,0 +1,13 @@
+package pers.lxl.mylearnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylearnprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MylearnprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/lxl/mylearnproject/MylearnprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.lxl.mylearnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylearnprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
