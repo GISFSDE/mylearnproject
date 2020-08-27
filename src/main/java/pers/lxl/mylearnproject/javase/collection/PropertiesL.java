3f7368f8bc39de5d32a7a953a311f094cdb5f362
passion7144 <714416426@qq.com>
2020-08-27 16:03:14 +0800

    lxl 8.27 1.4

--- src/main/java/pers/lxl/mylearnproject/javase/collection/PropertiesL.java
+++ src/main/java/pers/lxl/mylearnproject/javase/collection/PropertiesL.java
@@ -1,4 +1,17 @@
 package pers.lxl.mylearnproject.javase.collection;
 
+import java.io.FileOutputStream;
+import java.util.Properties;
+
 public class PropertiesL {
+    public static void main(String[] args) {
+        String f = "setting.properties";
+        Properties props = new Properties();
+        props.setProperty("language", "Java");
+//        props.store(new FileOutputStream("C:\\conf\\setting.properties"), "这是写入的properties注释");
+//        props.load(new java.io.FileInputStream(f));
+//        props.load(getClass().getResourceAsStream("setting.properties"));
+        String filepath = props.getProperty("last_open_file");
+        String interval = props.getProperty("auto_save_interval", "120");
+    }
 }
