b2c6bad518c016d086932e018c18afd615b0f64f
passion7144 <714416426@qq.com>
2020-09-04 17:22:57 +0800

    lxl 9.4 1.8

---
+++ src/main/java/pers/lxl/mylearnproject/programbase/designpatterns/creationalpattern/Hello.java
@@ -0,0 +1,10 @@
+package pers.lxl.mylearnproject.programbase.designpatterns.creationalpattern;
+/*创建型模式关注点是如何创建对象，--核心思想--是要把对象的创建和使用相分离，这样使得两者能相对独立地变换。
+创建型模式包括：
+工厂方法：Factory Method
+抽象工厂：Abstract Factory
+建造者：Builder
+原型：Prototype
+单例：Singleton*/
+public class Hello {
+}
