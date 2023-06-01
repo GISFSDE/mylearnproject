076a31704c2f33e022ec5855392aab8b4dca51cb
gisfsde <714416426@qq.com>
2023-06-01 12:40:52 +0800

    new jdk test modules

---
+++ newjdk/jdk11/src/main/java/com/gisfsde/jdk11/Jdk11Application.java
@@ -0,0 +1,16 @@
+package com.gisfsde.jdk11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author PC
+ */
+@SpringBootApplication
+public class Jdk11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk11Application.class, args);
+    }
+
+}
---
+++ newjdk/jdk20/src/main/java/com/gisfsde/jdk20/Jdk20Application.java
@@ -0,0 +1,13 @@
+package com.gisfsde.jdk20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk20Application.class, args);
+    }
+
+}
